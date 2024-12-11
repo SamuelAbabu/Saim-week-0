539bdaa272ef36065095ef8ed69a49f03ef8ccd3
SamuelAbabu <samuelababu@gmail.com>
2024-12-12 01:06:07 +0300

    Summary statistics

--- scripts/EDA_analysis.py
+++ scripts/EDA_analysis.py
@@ -1,8 +1,8 @@
 import pandas as pd  
 import numpy as np  
 # Load the dataset  
-#data = pd.read_csv('f:/አስዃላ/online/10 kifia Aim/data/benin-malanville.csv')  
-#data = pd.read_csv('f:/አስዃላ/online/10 kifia Aim/data/sierraleone-bumbuna.csv')
+data = pd.read_csv('f:/አስዃላ/online/10 kifia Aim/data/benin-malanville.csv')  
+data = pd.read_csv('f:/አስዃላ/online/10 kifia Aim/data/sierraleone-bumbuna.csv')
 data = pd.read_csv('f:/አስዃላ/online/10 kifia Aim/data/togo-dapaong_qc.csv')
 
 # Display the first few rows of the dataset  
